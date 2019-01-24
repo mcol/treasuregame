4907a7f6d4f3f7025e0165936c3eef7b5925def9
Marco Colombo <mar.colombo13@gmail.com>
2019-01-24 09:53:21 +0000

    Allow to exit from the MenuState by pressing escape.

--- core/src/mcol/treasuregame/states/MenuState.java
+++ core/src/mcol/treasuregame/states/MenuState.java
@@ -1,6 +1,7 @@
 package mcol.treasuregame.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -62,8 +63,15 @@ public class MenuState extends State {
         stage.addActor(exit);
     }
 
+    /** Handles the input. */
+    private void handleInput() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
+            Gdx.app.exit();
+    }
+
     @Override
     protected void update(float dt) {
+        handleInput();
         background.update(dt);
     }
 
