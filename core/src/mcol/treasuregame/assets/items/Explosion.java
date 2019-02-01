eea1c4c702d53265a44bc756b2d9c2ad9135b4d1
Marco Colombo <mar.colombo13@gmail.com>
2019-02-01 11:19:11 +0100

    Store the centre of the explosion as its position.

--- core/src/mcol/treasuregame/assets/items/Explosion.java
+++ core/src/mcol/treasuregame/assets/items/Explosion.java
@@ -13,9 +13,7 @@ public class Explosion extends AnimatedItem {
 
     /** Constructor. */
     public Explosion(float x, float y, int radius) {
-        super(x - radius * TreasureGame.TILESIZE,
-              y - radius * TreasureGame.TILESIZE,
-              0, Assets.explTexture, 0.1f, 0);
+        super(x, y, 0, Assets.explTexture, 0.1f, 0);
         this.radius = radius;
         CameraShake.set(0.4f, 0.2f * radius);
     }
@@ -23,10 +21,12 @@ public class Explosion extends AnimatedItem {
     @Override
     public void render(SpriteBatch sb) {
         TextureRegion frame = animation.getKeyFrame(stateTime);
+        float tx = x - radius * TreasureGame.TILESIZE;
+        float ty = y - radius * TreasureGame.TILESIZE;
         int diameter = radius * 2 + 1;
         for (int i = 0, end = diameter * diameter; i < end; i++)
-            sb.draw(frame, x + i / diameter * TreasureGame.TILESIZE,
-                           y + i % diameter * TreasureGame.TILESIZE, SIZE, SIZE);
+            sb.draw(frame, tx + i / diameter * TreasureGame.TILESIZE,
+                           ty + i % diameter * TreasureGame.TILESIZE, SIZE, SIZE);
     }
 
     @Override
